96ab56ede232f676511f75804140334821da50e9
JadenFicklin <jadendficklin@gmail.com>
2022-03-14 11:12:10 -0600

    updated jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 import "./App.css";
 import Display from "./components/Display";
 
 function App() {
-  const [info, setInfo] = useState("");
-
-  const buttonClick = async () => {
-    await axios
-      .get("/test")
-      .then((res) => setInfo(res.data[0][0].info))
-      .catch((err) => alert("No connection or data"));
-  };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        {info ? info : <p>Heroku Deployment Example</p>}
-        <button onClick={buttonClick}>Click here</button>
-        <Display />
-      </header>
+    <div className="app-header">
+      <Display />
     </div>
   );
 }
